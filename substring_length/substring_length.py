3fd2949ed9ba73099f9023c028bc041fd594591b
MateoLCS <mateusz.namysl0318@gmail.com>
2022-10-10 23:36:56 +0200

    Started creating longest substring search algorithm

---
+++ substring_length/substring_length.py
@@ -0,0 +1,29 @@
+
+def contains(character, array):
+    for i in range(len(array)):
+        if character == array[i]:
+            return True
+    return False
+
+test_input = "aab"
+
+
+def substring_length(s):
+    substr_length = 0
+    chars = []
+    possible_results = []
+    for i in range(len(s)):
+        if not contains(s[i], chars):
+            substr_length = substr_length +1
+            chars.append(s[i])
+        else:
+            possible_results.append(substr_length)
+            chars = []
+            substr_length = 0
+    possible_results.append(substr_length)
+
+    return max(possible_results)
+        
+                
+
+print(substring_length(test_input))
