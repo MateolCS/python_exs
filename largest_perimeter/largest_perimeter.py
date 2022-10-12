594f90ebce56c62002928dad148981e7e03d1c3f
MateoLCS <mateusz.namysl0318@gmail.com>
2022-10-12 23:20:22 +0200

    largest perimeter final version

--- largest_perimeter/largest_perimeter.py
+++ largest_perimeter/largest_perimeter.py
@@ -4,9 +4,12 @@ test_input = [1, 2, 1]
 def largest_perimeter(nums):
     nums.sort(reverse=True)
     print(nums)
-    if nums[0] >= (nums[1] + nums[2]):
-            return 0
-    return sum(nums)
+    max_perimeter = 0
+    for i in range(len(nums)-2):
+        if nums[i] < nums[i+1] + nums[i+2]:
+            if nums[i] + nums[i+1] + nums[i+2] > max_perimeter:
+                max_perimeter = nums[i] + nums[i+1] + nums[i+2] 
+    return max_perimeter
 
 print(largest_perimeter(test_input))
         
