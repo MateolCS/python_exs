1bbf9361b9b024d0b8ca22943f60b03cb703bbca
MateoLCS <mateusz.namysl0318@gmail.com>
2022-10-16 23:33:23 +0200

    Finished valid parentheses

--- valid_parenthesis/valid_parenthesis.py
+++ valid_parenthesis/valid_parenthesis.py
@@ -3,41 +3,19 @@
 test_input = "(){}[]"
 
 def valid_parenthesis( s ):
-    cases = {
-        '(': ')',
-        '{': '}',
-        '[': ']'}
-    i = 0
-    if len(s) % 2 != 0:
-        return False
+    stack = []
+    close_to_open = {")": "(", "}": "{", "]": "["}
 
-    while i < len(s):
-        if cases[s[i]] == s[len(s) - 1 - i]:
-            i += 1
-            break
+    for character in s:
+        if character in close_to_open:
+            if stack and stack[-1] == close_to_open[character]:
+                stack.pop()
+            else:
+                return False
         else:
-            return False
-             
-    return True
+            stack.append(character)
 
-def test(s):
-    open = [{'(': 0}, {'{': 0}, {'[': 0}]
-    close = [{')': 0}, {'}': 0}, {']': 0}]
-
-    for i in s:
-        for j in open:
-            if i in j:
-                j[i] += 1
-        for k in close:
-            if i in k:
-                k[i] += 1
-
-    for i in range(len(open)):
-        if list(open[i].values())[0] != list(close[i].values())[0]:
-            return False
-    return True
-
-print(test(test_input))
+        return True if not stack else False
     
 
 
